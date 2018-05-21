bcc6977c8f1539efe8d7cb2bee45aa82855cc91e
jmshnds <jmshnds@gmail.com>
2018-05-21 12:22:56 -0700

    Add gameObjects base class
    Applies to all objects in the game, food, snake, tail piece.

--- pysnake.py
+++ pysnake.py
@@ -16,6 +16,17 @@ BLOCK_S = 10
 BLOCK_W = WIDTH / BLOCK_S
 BLOCK_H = HEIGHT / BLOCK_S
 
+def process_snake_keys(snake, event):
+    # Check if a key has been pressed
+    if event.scancode is keycode['up']:
+        snake.changeDirection(direction['NORTH'])
+    elif event.scancode is keycode['down']:
+        snake.changeDirection(direction['SOUTH'])
+    elif event.scancode is keycode['left']:
+        snake.changeDirection(direction['WEST'])
+    elif event.scancode is keycode['right']:
+        snake.changeDirection(direction['EAST'])
+
 if __name__ == '__main__':
 
     pygame.init()
@@ -39,33 +50,20 @@ if __name__ == '__main__':
             # Check if the window has been closed
             if event.type == pygame.QUIT:
                 exit_game = True
-
-            # Check if a key has been pressed
             elif event.type == pygame.KEYDOWN:
-                if event.scancode is keycode['up']:
-                    snake.changeDirection(direction['NORTH'])
-                elif event.scancode is keycode['down']:
-                    snake.changeDirection(direction['SOUTH'])
-                elif event.scancode is keycode['left']:
-                    snake.changeDirection(direction['WEST'])
-                elif event.scancode is keycode['right']:
-                    snake.changeDirection(direction['EAST'])
+                process_snake_keys(snake, event) # Process arrow keys
 
             #print(event) # debug print events
 
-        # draw background
+        # Draw game objects
         screen.fill(color['white'])
+        snake.draw(draw, screen, color['red'], (BLOCK_S, BLOCK_S, BLOCK_S, BLOCK_S))
+        food.draw(draw, screen, color['blue'], (BLOCK_S, BLOCK_S, BLOCK_S, BLOCK_S))
 
-        # draw snake
-        for tail_piece in snake.tail:
-            #print(tail_piece.toString()) # print tailpiece information
-            draw.rect(screen, color['red'], (tail_piece.x*BLOCK_S, tail_piece.y*BLOCK_S, BLOCK_S, BLOCK_S))
-
-        # draw food
-        draw.rect(screen, color['blue'], (food.x*BLOCK_S, food.y*BLOCK_S, BLOCK_S, BLOCK_S))
-
+        # Move snake and check snake conditions
         snake.move()
-        # check if snake has moved off screen
+
+        # Check if the snake has moved off screen
         if snake.x < 0:
             snake.x = BLOCK_W
         elif snake.x >= BLOCK_W:
@@ -75,20 +73,24 @@ if __name__ == '__main__':
         elif snake.y >= BLOCK_H:
             snake.y = 0
 
-        # check if snake ran into tail
+        # Check if the snake ran into its tail
         for i in range(1, len(snake.tail)):
             if snake.x == snake.tail[i].x and snake.y == snake.tail[i].y:
-                print("Game over")
+                print("Game over: tail")
                 exit_game = True
 
-        # check if the food has been eaten
+        # Check if the snake has eaten a piece of food
         if snake.x == food.x and snake.y == food.y:
-            snake.grow(2)
-            # change position
-            food.x = randint(0, BLOCK_W-1)
-            food.y = randint(0, BLOCK_H-1)
+            if not food.isPoison:
+                snake.grow(2)
+                # change position
+                food.x = randint(0, BLOCK_W-1)
+                food.y = randint(0, BLOCK_H-1)
+            else:
+                print("Game Over: poison food")
 
-        pygame.display.update()
+        pygame.display.update() # Update game display
         clock.tick(12) # FPS
 
-    pygame.quit() # quit pygame instance
+    pygame.quit() # Quit pygame instance
+
--- snake.py
+++ snake.py
@@ -1,21 +1,24 @@
 
+from gameObject import gameObject
 from direction import direction
 
-class TailPiece:
+class TailPiece(gameObject):
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        super().__init__(x, y) # Super ctor with x y coord of tail piece
 
     def toString(self):
         return "TailPiece: (%d, %d)" % (self.x, self.y)
 
-class Snake:
+    # Draw tail piece
+    def draw(self, draw, screen, color, shape):
+        draw.rect(screen, color, (shape[0]*self.x, shape[1]*self.y, shape[2], shape[3]))
+
+class Snake(gameObject):
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.tail = [TailPiece(x,y)]
-        # direction of the head
-        self.direction = direction['NORTH'] 
+        super().__init__(x, y) # Super ctor with x y coor of the head
+
+        self.tail = [TailPiece(x,y)] # Tail piece list
+        self.direction = direction['NORTH'] # Direction of the head
 
     def changeDirection(self, new_dir):
         # do not allow backward direction changes
@@ -23,14 +26,14 @@ class Snake:
             self.direction is direction['SOUTH'] and new_dir is direction['NORTH'] or \
             self.direction is direction['WEST'] and new_dir is direction['EAST'] or \
             self.direction is direction['EAST'] and new_dir is direction['WEST']:
-            return
+            return # Do not change direction
         else:
             self.direction = new_dir
 
     def move(self):
-        lastPos = (self.x, self.y)
+        lastPos = (self.x, self.y) # Track last position of the head
 
-        # move head
+        # Move head
         if self.direction is direction['NORTH']:
             self.y -= 1
         elif self.direction is direction['EAST']:
@@ -40,18 +43,25 @@ class Snake:
         else:
             self.x -= 1
 
+        # Update tail piece coords for head
         self.tail[0].x = self.x
         self.tail[0].y = self.y
 
-        # then move each tail piece
+        # Then move each tail piece
         for i in range(1, len(self.tail)):
             tempPos = (self.tail[i].x, self.tail[i].y)
             self.tail[i].x = lastPos[0]
             self.tail[i].y = lastPos[1]
             lastPos = tempPos
 
+    # Grow tail by n pieces
     def grow(self, n):
-        # grow tail by n
         lastPiece = self.tail[len(self.tail)-1]
         for i in range(n):
             self.tail.append(TailPiece(lastPiece.x, lastPiece.y))
+
+    # Draw snake
+    def draw(self, draw, screen, color, shape):
+        for t in self.tail:
+            t.draw(draw, screen, color, shape)
+
